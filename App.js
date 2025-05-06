7a87c7eac5934819e8274bfaddf794b1008102f6
flaviaglenda <flaviaglenda15@gmail.com>
2025-05-06 08:45:41 -0300

    commit fla drawer

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import realizarLogin from './src/screens/realizarLogin';
+import realizarCadastro from './src/screens/realizarCadastro';
+import telaInicial from './src/screens/telaInicial';
+import telaGaleria from './src/screens/galeria';
+import telaAgendamento from './src/screens/agendamento';
+import telaUpload from './src/screens/realizarUpload';
+
+const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator initialRouteName="Galeria">
+      <Drawer.Screen name="Início" component={telaInicial} />
+      <Drawer.Screen name="Galeria" component={telaGaleria} />
+      <Drawer.Screen name="Agendamento de ensaios" component={telaAgendamento} />
+      <Drawer.Screen name="Upload de fotos" component={telaUpload} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
+          <Stack.Screen name="Login" component={realizarLogin} />
+          <Stack.Screen name="Cadastro" component={realizarCadastro} />
+          <Stack.Screen name="Principal" component={DrawerNavigator} />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- src/screens/realizarLogin.js
+++ src/screens/realizarLogin.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 export default function App() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>tela de realizar login</Text>
+      <Button title="Ir para app" onPress={() => navigation.navigate('Principal')} />
       <StatusBar style="auto" />
     </View>
   );
