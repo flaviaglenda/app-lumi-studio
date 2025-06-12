a2ba570d40f6e4b85093bc467ab6f027a6763514
JuliaConconi <conconijulia@gmail.com>
2025-06-12 11:00:49 -0300

    criacao aws

---
+++ awsConfig.js
@@ -0,0 +1,12 @@
+import AWS from "aws-sdk";
+
+AWS.config.update ({
+    accessKeyId: "ASIAYQFMZ7UFYAFSLTGC",
+    secretAccessKey: "4Jb9+cNmbPm1OdtAI49NQkJLup1SG0I8qka2u15G",
+    sessionToken: "IQoJb3JpZ2luX2VjEBMaCXVzLXdlc3QtMiJIMEYCIQCwL4zfp8BpKL0FOTV/NnrxDvI84qIn3O5VZGaxuK6zkAIhAIkeVt+R0aoleH3bw6rMEc1VntN3JFktTtfL6gYcIUbvKrsCCOz//////////wEQABoMNTg0NDc3OTY1NTc5IgwphPB5Y9CAMQuPuJkqjwKDNcNbM7iNp4DNSEbS9l2S/1r6r7Oh02NmVxcpAhu7tdYV8gtanYTZX21OPJ7IV1tGAZJjVVHrCDyUG7I/7tZyxvuLo9p/Omu4pdppPa2W+0w1JZ3acs4AIe7Xm1b1SAZSosmUvSWrPMEe+1m3LCeO19FRfe33Qe3qpayCTJr3K4m1Jso9XrYfCXlApfNv3yD7MEeYzMljkBCgFBBI4IpDQJnDecg92gpnzBUSPBKmsIpypvTEYI3B/EHjmfy2qY816qrb4PcxxcE6JUqiOCh82pc9BRvWD36Gj/4pp9cvluFFEaS1581uSEUmxlMAYbPwRJThoPFoZIthuB5CUapY+8kegd2CjCMq3Bf2o8ioMJfkqsIGOpwBk6ftFEZ7aUPNvWN3x7Guf3CAQ6I5l5J1Led0ghCEcIlS2kd14cMie/UYgegBBXFWcFiU7+jARZBjQ9R+C+k2n17Xx8dfT7p122yXBmcr0yqzQr1bd0h3lltGKTUedNTBmzbZciDvbrBD/fphTH9clX6uWY68hmh+OZetDJ2J4xiCG+EGn1hIEDDgJgrGdqdeA5gSvQVHCND1dCvL",
+    region: "us-east-1",
+});
+
+const s3 = new AWS.S3();
+
+export default s3;
